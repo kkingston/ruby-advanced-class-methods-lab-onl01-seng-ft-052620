e6a19a3fab9578f03b7fd8d6ebc6702e3f43ecb9
Kayla Kingston <kkingston09@gmail.com>
2020-06-08 22:02:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,16 @@
 
 class Song
-  
+  attr_accessor :name, :artist_name
   @@all = []
   
   def self.create 
     song = self.new 
     @@all << song
-    self 
+    name
+  end
+  
+  def save
+    self.class.all << self
   end
 end 
 
